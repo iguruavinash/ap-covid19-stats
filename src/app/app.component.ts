5f1cc4ea072b77f0829dc6c6d091f79b86845ae9
iguruavinash <iguruavinash@gmail.com>
2021-04-26 13:25:32 +0530

    Removed console logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,32 @@
-import { Component, VERSION } from '@angular/core';
-import { AppService } from './app.service';
+import { Component, VERSION } from "@angular/core";
+import { AppService } from "./app.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ],
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
   providers: [AppService]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
   apCovid19Data: Array<any> = [];
-  constructor(private appService: AppService) {
-  }
+  constructor(private appService: AppService) {}
 
   ngOnInit() {
-    this.appService.getCovidData().subscribe((response) => {
-      if(response['Andhra Pradesh1'] && response['Andhra Pradesh']['districtData']) {
-        const apData = response['Andhra Pradesh']['districtData'];
-      this.apCovid19Data = Object.keys(apData).map(key => ({ name: key, value: apData[key] }));
+    this.appService.getCovidData().subscribe(response => {
+      if (
+        response["Andhra Pradesh"] &&
+        response["Andhra Pradesh"]["districtData"]
+      ) {
+        const apData = response["Andhra Pradesh"]["districtData"];
+        this.apCovid19Data = Object.keys(apData).map(key => ({
+          name: key,
+          value: apData[key]
+        }));
       } else {
         this.apCovid19Data = [];
       }
-      console.log(this.apCovid19Data);
+      // console.log(this.apCovid19Data);
     });
   }
-  
 }

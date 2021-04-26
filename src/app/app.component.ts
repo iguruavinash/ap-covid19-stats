c813211b294df828a048e20977e6898ace9bee57
iguruavinash <iguruavinash@gmail.com>
2021-04-26 13:23:44 +0530

    API response integration and data view modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component, VERSION } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  providers: [AppService]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  apCovid19Data: Array<any> = [];
+  constructor(private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.appService.getCovidData().subscribe((response) => {
+      if(response['Andhra Pradesh1'] && response['Andhra Pradesh']['districtData']) {
+        const apData = response['Andhra Pradesh']['districtData'];
+      this.apCovid19Data = Object.keys(apData).map(key => ({ name: key, value: apData[key] }));
+      } else {
+        this.apCovid19Data = [];
+      }
+      console.log(this.apCovid19Data);
+    });
+  }
+  
 }
